f6d60c284ccdca0e8adb8c1a0c8c708ecfffe218
Philip <diazphi@gmail.com>
2023-01-25 09:52:46 +0800

    updates collection filter

--- src/components/elements/CollectionFilter.client.jsx
+++ src/components/elements/CollectionFilter.client.jsx
@@ -28,7 +28,6 @@ export function CollectionFilter({minPrice, maxPrice}) {
     }
     
     const onFilterParam = event => {
-        console.log(event);
         event.target.closest('.nested-list').classList.toggle('show');
     }
 
@@ -52,7 +51,6 @@ export function CollectionFilter({minPrice, maxPrice}) {
     }
 
     const onFilterPriceParam = event => {
-        console.log(event.target);
         if(event.target.closest('.filter-price')) {
             event.target.closest('.collection-price-container').classList.toggle('show');
         }        
